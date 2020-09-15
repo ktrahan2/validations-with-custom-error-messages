af10588a65034b3f383756cc44fa1d9eab50f04d
Kyle Trahan <ktrahan2@gmail.com>
2020-09-15 13:04:47 -0400

    lesson done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,4 +5,23 @@ class DogsController < ApplicationController
     render json: @dogs
   end
 
+  def create
+    @dog = Dog.new(dog_params)
+
+    if @dog.valid?
+      @dog.save
+      render json: @dog
+    else
+      render json: {message: @dog.errors.full_messages}
+    end
+  end
+
+  private 
+
+  def dog_params
+    params.require(:dog).permit(:name, :age)
+  end
+
+
+
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,5 @@
 class Dog < ApplicationRecord
+
+    validates :name, presence: true, uniqueness: true
+    
 end
